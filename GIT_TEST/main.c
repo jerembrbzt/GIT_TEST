1218b14d91200649728611ac427915368d0e0f19
jerembrbzt <barbezat.jeremy@gmail.com>
2021-06-22 13:54:49 +0200

    Initial Commit

---
+++ GIT_TEST/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GIT_TEST
+//
+//  Created by Jeremy Barbezat on 22.06.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
